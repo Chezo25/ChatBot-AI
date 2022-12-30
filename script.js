9da7342cc6e82acede126139484b45dd0d215bfe
Chezo25 <47767271+Chezo25@users.noreply.github.com>
2022-12-29 23:27:09 -0400

    -add loader timer

--- script.js
+++ script.js
@@ -8,5 +8,14 @@ let loadInterval;
 
 //the loader for waiting on the AI to return answers
 function loader(element) {
-  '...';
+  element.textContent = '';
+
+  loadInterval = setInterval(( => {
+    // Update the text content of the loading indicator
+    element.textContent +='.';
+
+    if (element.textContent === '....'){
+      element.textContent = '';
+    }
+  }, 500))
 }
