2007f7ed58559cf8d9ccdab6376083ac0f79c038
Chezo25 <47767271+Chezo25@users.noreply.github.com>
2022-12-29 23:08:08 -0400

    -add loade function

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 import bot from './public/bot.svg';
 import user from './public/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+//the loader for waiting on the AI to return answers
+function loader(element) {
+  '...';
+}
