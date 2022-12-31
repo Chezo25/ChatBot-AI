c662d2fbf841b5794e3a34978fd58ee1b488ec47
Chezley Stoddard <47767271+Chezo25@users.noreply.github.com>
2022-12-31 22:12:33 +0000

    -add api model to server

--- server/server.js
+++ server/server.js
@@ -22,5 +22,21 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-    try {} catch (error) {}
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openapi.createCompletion({
+          model: "text-davinci-003",
+          prompt: `${prompt}`,
+          temperature: 0.5, // Higher values means the model will take more risks.
+          max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+          top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+          frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+          presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+        });
+
+         res.status(200).send({
+           bot: response.data.choices[0].text,
+         });
+    } catch (error) {}
 })
