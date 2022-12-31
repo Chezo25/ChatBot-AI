37d7c48dd78f6976ee7ad23c95296507fdfa9a08
Chezley Stoddard <47767271+Chezo25@users.noreply.github.com>
2022-12-31 21:57:35 +0000

    -building server up

--- server/server.js
+++ server/server.js
@@ -5,6 +5,22 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-const configuration = new Configuration
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openapi = new OpenAIApi(configuration);
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX!!!',});
+});
+
+app.post('/', async (req, res) => {
+    try {} catch (error) {}
+})
